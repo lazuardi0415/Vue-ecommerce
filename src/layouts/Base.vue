273a42133a4e080c661c3d8c229157d6e87d276a
lazuardi0415 <51481549+lazuardi0415@users.noreply.github.com>
2022-02-19 17:06:34 +0700

    add header/footer, modified navbar (WIP)

--- src/layouts/Base.vue
+++ src/layouts/Base.vue
@@ -1,30 +1,56 @@
 <template>
   <div>
-    <div>
-      <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
-        <router-link class="navbar-brand" to="/">CS-eCommerce</router-link>
-        <div class="ml-auto">
-          <div v-if="user.photoURL">
-            <img :src="user.photoURL" class="img-thumbnail profile-image" alt />
-            <router-link class="btn btn-primary my-2 my-sm-0" to="/cart">
+    <div style="background: hsl(0, 0%, 10%)">
+      <div class="container">
+        <div>
+          <div
+            class="jumbotron jumbotron-fluid m-0 py-4 text-left"
+            style="background: hsl(0, 0%, 10%); color: white"
+          >
+            <div>
+              <h1 class="m-0 p-0" style="font-weight: 900">KEYSTROKE, INC.</h1>
+              <span class="catchphrase">One stop keyboard market</span>
+            </div>
+          </div>
+          <div>
+            <nav class="navbar px-0">
+              <router-link class="nav-link" to="/">Home</router-link>
+              <router-link class="nav-link" to="/">Products</router-link>
+              <!-- <div>
+            <div v-if="user.photoURL">
               <img
-                src="https://pngimg.com/uploads/shopping_cart/shopping_cart_PNG38.png"
-                width="50"
+                :src="user.photoURL"
+                class="img-thumbnail profile-image"
                 alt
               />
-              <span class="badge badge-danger badge-pill">{{
-                cart.length
-              }}</span>
-            </router-link>
+              <router-link class="btn btn-primary my-2 my-sm-0" to="/cart">
+                <img
+                  src="https://pngimg.com/uploads/shopping_cart/shopping_cart_PNG38.png"
+                  width="50"
+                  alt
+                />
+                <span class="badge badge-danger badge-pill">{{
+                  cart.length
+                }}</span>
+              </router-link>
+            </div>
+            <router-link v-else class="btn btn-primary my-2 my-sm-0" to="/login"
+              >Login</router-link
+            >
+          </div> -->
+            </nav>
           </div>
-          <router-link v-else class="btn btn-primary my-2 my-sm-0" to="/login"
-            >Login</router-link
-          >
         </div>
-      </nav>
+      </div>
     </div>
     <div class="page-container">
       <router-view />
+    </div>
+    <div class="jumbotron jumbotron-fluid m-0 py-3">
+      <div class="container">
+        <h1 class="display-4">Footer</h1>
+        <p class="lead">Catchphrase goes here</p>
+      </div>
     </div>
   </div>
 </template>
@@ -41,6 +67,12 @@ export default {
 </script>
 
 <style>
+span.catchphrase {
+  font-family: "IBM Plex Sans", sans-serif;
+  font-weight: 300;
+  font-size: 18px;
+}
+
 nav {
   background-color: hsl(0, 0%, 10%);
 }
@@ -71,5 +103,17 @@ input {
 }
 .btn:focus {
   background: lightcoral;
+}
+
+h1 {
+  font-family: "IBM Plex Mono", monospace;
+}
+
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: "IBM Plex Sans", sans-serif;
 }
 </style>
